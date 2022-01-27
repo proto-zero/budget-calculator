075e078ae95129b532badf21f2c7d2ee636d4455
Sean Lenhart <theseanlenhart@gmail.com>
2022-01-26 21:40:47 -0500

    add outline to readme, test api returns

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 - I stripped App.js and added a components folder with a new component, Outline, which I will use to sketch the overall project in order to help decide what components I will need. I placed Outline in App.
 
 - I want to get the API up and running so I can do some tests with it. Using the supplied documentation I added firebase to the project.
+
+## Outline
+- Ask the User for their budget
+- Save the budget in the state
+- Have a remaining balance in the state, budget - item cost
+- Have a dropdown for yard items
+- When users select a yard item, list chosen items like a receipt
+- subtract item cost from the budget and update the remaining balance
+- change remaining balance color to reflect when approaching last 10% of budget or overbudget
--- src/components/outline.js
+++ src/components/outline.js
@@ -1,9 +1,41 @@
 import react from "react";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+// "items" collection interface
+// Each document in the collection looks like this:
+// interface Item {
+// type: string;
+// name: string;
+// lowPrice: number;
+// highPrice: number;
+// }
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD7NUVfrImccSo8FuCBG7bXVk0oLFqgE-k",
+    authDomain: "yardzen-demo.firebaseapp.com",
+    databaseURL: "https://yardzen-demo.firebaseio.com",
+    projectId: "yardzen-demo",
+    storageBucket: "yardzen-demo.appspot.com",
+    messagingSenderId: "509183652730",
+    appId: "1:509183652730:web:ba2208f7d8e0882f009cc3"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function getNames(db) {
+    const nameCol = collection(db, 'items');
+    const nameSnapshot = await getDocs(nameCol);
+    const nameList = nameSnapshot.docs.map(doc => doc.data());
+    return nameList;
+}
 
 function Outline() {
     return (
         <div>
             hello there
+            {console.log(getNames(db))}
         </div>
     );
 }
