b8259ffa1af94028a19d1b5c025ba6a9d7ce0cfb
Sean Lenhart <theseanlenhart@gmail.com>
2022-01-28 22:43:42 -0500

    update Ex cards with Yardzen styling. Add selector with styling, comment out button. Add state for price selection

--- src/components/Ex.js
+++ src/components/Ex.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import './Ex.css'
 
 function Ex(props) {
+    const [price, setPrice] = useState(0);
+
+    function priceHandler(e) {
+        let itemPrice = (e.target.value);
+        setPrice(itemPrice);
+    }
     return (
         <div className="ex">
             <h3>{props.type}</h3>
             <h4>{props.name}</h4>
-            <p>Low Price</p>
-            <li>{props.lowprice}</li>
-            <p>High Price</p>
-            <li>{props.highprice}</li>
+
+            <select onChange={priceHandler}>
+                <option>Price</option>
+                <option value={props.lowprice}>{props.lowprice}</option>
+                <option on value={props.highprice}>{props.highprice}</option>
+            </select>
+
+            {/* <button value={props.lowprice} onClick={priceHandler}>
+                <div>Low Price</div>
+                <div>{props.lowprice}</div>
+            </button>
+            <button value={props.highprice} onClick={priceHandler}>
+                <div>High Price</div>
+                <div>{props.highprice}</div>
+            </button> */}
+            {price}
         </div>
     );
 }
